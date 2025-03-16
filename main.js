8b4e0a7f781220a5db762ddd6fce7f00c436be60
abdesslemhamani <abdesslemhamani@gmail.com>
2025-03-16 15:45:20 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+
+const buttons =document.querySelectorAll("button");
+const resultEl =document.getElementById("result");
+const playerScoreEl =document.getElementById("user-score");
+const computerScoreEl =document.getElementById("computer-score");
+let playerScore = 0 ;
+let computerScore = 0 ;
+
+buttons.forEach((button) => {
+    button.addEventListner("click",() => {
+        const result = playRound(button.id,computerPlay());
+        resultEl.textContent = result;
+    });
+});
+
+function computerPlay(){
+    const choices =["rock","paper","scissors"];
+    const randomChoice = Math.floor(Math.random()*choices.length);
+}
